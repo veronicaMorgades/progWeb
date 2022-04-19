7be9069a63e0dc7f9938fdcae0f1badece7bdfad
veronicaMorgades <80260631+veronicaMorgades@users.noreply.github.com>
2022-04-19 08:57:39 -0300

    Add files via upload

---
+++ Contatcto.java
@@ -0,0 +1,31 @@
+public class Contacto {
+
+	private String nombre;
+	private int numero;
+	private String email;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
